7b95e756b6383058ffa94d18ec1212421178fb27
George <andrea.xhorxhi@gmail.com>
2023-03-14 11:13:47 +0000

    Create Post and PostImage model to upload multiple images

--- photoblog/admin.py
+++ photoblog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Post, PostImage
 
-# Register your models here.
+
+class PostImageInline(admin.TabularInline):
+    model = PostImage
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    inlines = [PostImageInline]
+
+
+admin.site.register(PostImage)
--- photoblog/models.py
+++ photoblog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+import cloudinary
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    cover_image = CloudinaryField('image', default='placeholder')
+
+    def __str__(self):
+        return self.title
+
+
+class PostImage(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='post_images')
+    image = CloudinaryField('image')
+
+    def __str__(self):
+        return self.post.title
