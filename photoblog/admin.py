cc053ef1b954afcf6c3af644d7eddb75d654aecc
George <andrea.xhorxhi@gmail.com>
2023-03-14 11:27:43 +0000

    Register Comment model

--- photoblog/admin.py
+++ photoblog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, PostImage
+from .models import Post, PostImage, Comment
 
 
 class PostImageInline(admin.TabularInline):
@@ -11,4 +11,9 @@ class PostAdmin(admin.ModelAdmin):
     inlines = [PostImageInline]
 
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'created_at')
+
+
+admin.site.register(Comment, CommentAdmin)
 admin.site.register(PostImage)
