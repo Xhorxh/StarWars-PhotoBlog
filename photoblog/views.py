ec1aa570dac162144df2dfd902c452f77f92c0f8
George <andrea.xhorxhi@gmail.com>
2023-03-15 15:33:10 +0000

    Add django-crispy-forms to requirements.txt

--- photoblog/views.py
+++ photoblog/views.py
@@ -20,4 +20,23 @@ class PostDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         context['comments'] = self.object.comments.all().order_by('-created_at')
         context['comment_form'] = CommentForm()
+        context['commented'] = False
         return context
+
+    def post_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comments'] = self.object.comments.all().order_by('-created_at')
+        context['comment_form'] = CommentForm()
+        context['commented'] = True
+        return context
+
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+        else:
+            comment_form = CommentForm()    
