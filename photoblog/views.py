820d64c22fea5b63afae9019067488fd67e0b79a
George <andrea.xhorxhi@gmail.com>
2023-03-15 17:01:20 +0000

    Edit files to add the functionality to comment as a user

--- photoblog/views.py
+++ photoblog/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import ListView, DetailView
-from .models import Post
+from .models import Post, Comment
 from .forms import CommentForm
 
 
@@ -20,23 +20,19 @@ class PostDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         context['comments'] = self.object.comments.all().order_by('-created_at')
         context['comment_form'] = CommentForm()
-        context['commented'] = False
         return context
 
-    def post_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['comments'] = self.object.comments.all().order_by('-created_at')
-        context['comment_form'] = CommentForm()
-        context['commented'] = True
-        return context
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        context = self.get_context_data(object=self.object)
 
-        comment_form = CommentForm(data=request.POST)
-
+        comment_form = CommentForm(request.POST)
         if comment_form.is_valid():
-            comment_form.instance.email = request.user.email
-            comment_form.instance.name = request.user.username
             comment = comment_form.save(commit=False)
-            comment.post = post
+            comment.post = self.object
+            comment.author = request.user
             comment.save()
-        else:
-            comment_form = CommentForm()    
+            return self.render_to_response(context)
+
+        context['comment_form'] = comment_form
+        return self.render_to_response(context)
